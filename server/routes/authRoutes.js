743345dcaee8b56999011533a4a732ed55475e14
abrar-faseeh01 <abrarfaseeh01gmail.com>
2025-07-18 12:30:11 +0600

    add authentication routes and update error/response utility exports

---
+++ server/routes/authRoutes.js
@@ -0,0 +1,11 @@
+import { Router } from 'express';
+import { login, logout, register } from '../controllers/authController.js';
+
+
+const authRoutes = Router();
+
+authRoutes.post('/register',register);
+authRoutes.post('/login',login);
+authRoutes.post('/logout',logout);
+
+export default authRoutes;
--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ import cors from 'cors';
 import 'dotenv/config';
 import express from 'express';
 import connectDB from './config/mongodb.js';
+import authRoutes from './routes/authRoutes.js';
 
 
 const app = express();
@@ -14,9 +15,12 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(cors({credentials: true}))
 
+// API endpoints
 app.get('/',(req,res)=>{
     res.send("API is  working")
 })
+
+app.use('/api/auth',authRoutes)
 
 app.listen(port,()=>{
     console.log(`Server is running on PORT ${port}`);
