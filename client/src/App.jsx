aed805b8f214e80826b02ede5c0ba393fefb05df
abrar-faseeh01 <abrarfaseeh01gmail.com>
2025-08-05 18:14:43 +0600

    add routing and create main pages for Home, Login, Email Verification, and Reset Password

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,20 @@
+import { Route, Routes } from "react-router-dom";
+import EmailVerify from "./pages/EmailVerify.jsx";
+import Home from "./pages/Home.jsx";
+import Login from "./pages/Login.jsx";
+import ResetPassword from "./pages/ResetPassword.jsx";
+
 const App = () => {
-  return <div></div>;
+  return (
+    <div>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/email-verify" element={<EmailVerify />} />
+        <Route path="/reset-password" element={<ResetPassword />} />
+      </Routes>
+    </div>
+  );
 };
 
 export default App;
---
+++ client/src/pages/EmailVerify.jsx
@@ -0,0 +1,9 @@
+const EmailVerify = () => {
+  return (
+    <div>
+      <h1>Verify Email</h1>
+    </div>
+  );
+};
+
+export default EmailVerify;
